f22f3c17102f35fda1efbeab4d5444fb460b17ba
emre erol <emre_erol17@hotmail.com>
2023-09-29 08:56:07 +0300

    Proje dosyası ekle.

---
+++ 896. Monotonic Array/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        if (nums.Length == 1) return true;
+
+        var result = nums[nums.Length - 1] - nums[0];
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (result >= 0 && nums[nums.Length - 1] - nums[0] >= 0)
+            {
+                if (nums[i + 1] >= nums[i]) continue;
+                else return false;
+            }
+            else
+            {
+                if (nums[i + 1] <= nums[i]) continue;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
